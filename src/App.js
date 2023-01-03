aab4dd80268472b514265fe57e624a756b09a0cf
djx-ctrl <cras.dtravis@gmail.com>
2023-01-03 11:41:08 -0800

    - As a user, when I click on one of the question marks an alert appears with the index position of that question mark in the array.

--- src/App.js
+++ src/App.js
@@ -14,13 +14,17 @@ const App = () => {
     "?"
   ])
 
+  const handleGamePlay = (clickedSquare) => {
+    alert(clickedSquare)
+  }
+
   return (
     <>
       <h1>Treasure Hunt Game</h1>
       <div className="board">
-        {board.map(square => {
+        {board.map((square, index) => {
         return(
-        <Square square={square}/>
+        <Square square={square} index={index} handleGamePlay={handleGamePlay}/>
         )
       })}
       
